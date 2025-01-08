22ee9877fc9903c01a86cf1dfaee313d9577becb
Mishal <131391958+Mishel-07@users.noreply.github.com>
2025-01-08 19:09:04 +0530

    Update bing.go

--- settings/bing.go
+++ settings/bing.go
@@ -14,7 +14,7 @@ type BingResponse struct {
 }
 
 func SearchBing(query string) (BingResponse, error) {
-    url := fmt.Sprintf("https://horrid-api.vercel.app/images?page=7&query=%s", query)
+    url := fmt.Sprintf("https://horrid-api.vercel.app/images?page=10&query=%s", query)
     resp, err := http.Get(url)
     if err != nil {
         return BingResponse{}, err
