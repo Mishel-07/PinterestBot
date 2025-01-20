532e20011d1886909445022a12194e6b82b38079
Mishal <131391958+Mishel-07@users.noreply.github.com>
2025-01-20 21:02:30 +0530

    🙊

--- settings/start.go
+++ settings/start.go
@@ -1,18 +1,24 @@
 package settings
 
-import (
-  "github.com/PaulSonOfLars/gotgbot/v2"
-  "github.com/PaulSonOfLars/gotgbot/v2/ext"
+import (	
+        "github.com/PaulSonOfLars/gotgbot/v2"
+        "github.com/PaulSonOfLars/gotgbot/v2/ext"
 )
 
 
 func Start(b *gotgbot.Bot, ctx *ext.Context) error {    
-    message := ctx.Message
-	
-    _, err := message.Reply(b, "<b>Hey, I'm PinterestBot. You can search for Pinterest videos or photos, and you can download them too. I can also provide Google images, Bing images, etc.</b>\n\n<b>Commands:</b>\n\n/pinterest - search and download pinterest image\n/wallpaper - wallpaper search\n/img - from bing image\n\n<b>Tools:</b>\n\nSend me a Pinterest url I'll give that photo/video", &gotgbot.SendMessageOpts{ParseMode: gotgbot.ParseModeHTML})
-    if err != nil {
-        return nil
-    }
-
-    return nil 
+        message := ctx.Message
+        if message.Chat.Type == "private" {
+                return nil
+        }
+	buttons := gotgbot.InlineKeyboardMarkup{
+		InlineKeyboard: [][]gotgbot.InlineKeyboardButton{
+			{
+				{Text: "Source", Url: "https://github.com/Mishel-07/PinterestBot"},
+				{Text: "Support", Url: "https://t.me/XBOTSUPPORTS"},
+			},
+		},
+	}        
+        message.Reply(b, "<b>Hey, I'm PinterestBot. You can search for Pinterest videos or photos, and you can download them too. I can also provide Google images, Bing images, etc.</b>\n\n<b>Commands:</b>\n\n/pinterest - search and download pinterest image\n/wallpaper - wallpaper search\n/img - from bing image\n\n<b>Tools:</b>\n\nSend me a Pinterest url I'll give that photo/video", &gotgbot.SendMessageOpts{ParseMode: gotgbot.ParseModeHTML, ReplyMarkup: buttons})    
+        return nil 
 }
