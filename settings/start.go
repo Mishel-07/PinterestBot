954976b1118741889b9568371be8bb9b48f4609f
Mishal <131391958+Mishel-07@users.noreply.github.com>
2025-01-20 21:24:07 +0530

    Update start.go

--- settings/start.go
+++ settings/start.go
@@ -9,7 +9,7 @@ import (
 
 func Start(b *gotgbot.Bot, ctx *ext.Context) error {    
         message := ctx.Message
-        if message.Chat.Type == "private" {
+        if message.Chat.Type != "private" {
                 return nil
         }
 	buttons := gotgbot.InlineKeyboardMarkup{
@@ -20,7 +20,7 @@ func Start(b *gotgbot.Bot, ctx *ext.Context) error {
 			},
 		},
 	}              
-	_, _, e := message.Reply(b, "<b>Hey, I'm PinterestBot. You can search for Pinterest videos or photos, and you can download them too. I can also provide Google images, Bing images, etc.</b>\n\n<b>Commands:</b>\n\n/pinterest - search and download pinterest image\n/wallpaper - wallpaper search\n/img - from bing image\n\n<b>Tools:</b>\n\nSend me a Pinterest url I'll give that photo/video", &gotgbot.SendMessageOpts{ParseMode: gotgbot.ParseModeHTML, ReplyMarkup: buttons})    
+	_, e := message.Reply(b, "<b>Hey, I'm PinterestBot. You can search for Pinterest videos or photos, and you can download them too. I can also provide Google images, Bing images, etc.</b>\n\n<b>Commands:</b>\n\n/pinterest - search and download pinterest image\n/wallpaper - wallpaper search\n/img - from bing image\n\n<b>Tools:</b>\n\nSend me a Pinterest url I'll give that photo/video", &gotgbot.SendMessageOpts{ParseMode: gotgbot.ParseModeHTML, ReplyMarkup: buttons})    
         if e != nil {
 		fmt.Println(e)
 	}
