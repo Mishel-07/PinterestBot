b2640af31ac36bc0f9677c83ec2895fbf1545aab
Mishal <131391958+Mishel-07@users.noreply.github.com>
2025-02-11 18:33:00 +0530

    Update bing.go

--- settings/bing.go
+++ settings/bing.go
@@ -1,58 +1,36 @@
 package settings 
 
 import (
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "net/http"
+        "encoding/json"
+        "fmt"
+        "io/ioutil"
+        "net/http"
 )
 
 type BingResponse struct {
-    Result []struct {
-        URL string `json:"images"`
-    } `json:"result"` 
+        Result []struct {
+                URL string `json:"images"`
+        } `json:"result"` 
 }
 
-func SearchBing(query string) (BingResponse, error) {
-    url := fmt.Sprintf("https://horrid-api.vercel.app/images?page=10&query=%s", query)
-    resp, err := http.Get(url)
-    if err != nil {
-        return BingResponse{}, err
-    }
-    defer resp.Body.Close()
+func SearchBing(query string, count string) (BingResponse, error) {
+        url := fmt.Sprintf("https://horrid-api.vercel.app/images?page=%s&query=%s", count, query)
+        resp, err := http.Get(url)
+        if err != nil {
+                return BingResponse{}, err
+        }
+        defer resp.Body.Close()
 
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        return BingResponse{}, err
-    }
+        body, err := ioutil.ReadAll(resp.Body)
+        if err != nil {
+                return BingResponse{}, err
+        } 
 
-    var result BingResponse
-    err = json.Unmarshal(body, &result)
-    if err != nil {
-        return BingResponse{}, err
-    }
+        var result BingResponse
+        err = json.Unmarshal(body, &result)
+        if err != nil {
+                return BingResponse{}, err
+        }
 
-    return result, nil
-}
-
-func SearchBingInline(query string) (BingResponse, error) {
-    url := fmt.Sprintf("https://horrid-api.vercel.app/images?page=40&query=%s", query)
-    resp, err := http.Get(url)
-    if err != nil {
-        return BingResponse{}, err
-    }
-    defer resp.Body.Close()
-
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        return BingResponse{}, err
-    }
-
-    var result BingResponse
-    err = json.Unmarshal(body, &result)
-    if err != nil {
-        return BingResponse{}, err
-    }
-
-    return result, nil
+        return result, nil
 }
