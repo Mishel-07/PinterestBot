5c3708de9d70158cfeda249eee89b85d63c2baee
Mishal <131391958+Mishel-07@users.noreply.github.com>
2025-02-14 18:18:59 +0530

    valentines day munji

--- settings/pinterestdl.go
+++ settings/pinterestdl.go
@@ -1,31 +1,30 @@
 package settings 
 
 import (
- "encoding/json"
- "io/ioutil"
- "net/http"
+        "encoding/json"
+        "io/ioutil"
+        "net/http"
 )
 
 func PinterestDownload(link string) (string, error) {
-  url := "https://horrid-api.vercel.app/download_pin?url=" + link
-  resp, err := http.Get(url)
-  if err != nil {
-   return "", err
-  }
-  defer resp.Body.Close()
+        url := "https://horrid-api.vercel.app/download_pin?url=" + link
+        resp, err := http.Get(url)
+        if err != nil {
+                return "", err
+        }
+        defer resp.Body.Close()
  
-  body, err := ioutil.ReadAll(resp.Body)
-  if err != nil {
-   return "", err
-  }
+        body, err := ioutil.ReadAll(resp.Body)
+        if err != nil {
+                return "", err
+        }
 
-  var data struct {
-   Link string `json:"link"`
-  }
-  err = json.Unmarshal(body, &data)
-  if err != nil {
-   return "", err
-  }
-
-  return data.Link, nil
- }
+        var data struct {
+                Link string `json:"link"`
+        }
+        err = json.Unmarshal(body, &data)
+        if err != nil {
+                return "", err
+        }
+        return data.Link, nil
+}
