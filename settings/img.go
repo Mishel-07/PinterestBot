243ca1eaad82dce9c937cb703c9932b76aaf099b
Mishal <131391958+Mishel-07@users.noreply.github.com>
2025-02-14 18:14:48 +0530

    valentines day munji

--- settings/img.go
+++ settings/img.go
@@ -1,36 +1,37 @@
 package settings 
 
 import (
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "net/http"
+        "encoding/json"
+        "fmt"
+        "io/ioutil"
+        "net/http"
 )
 
 type PinterestResponse struct {
-    Data []struct {
-        URL string `json:"url"`
-    } `json:"data"` 
+        Data []struct {
+                URL string `json:"url"`
+        } `json:"data"` 
 }
 
 func SearchPinterest(query string) (PinterestResponse, error) {
-    url := fmt.Sprintf("https://horrid-api.vercel.app/pinterest?query=%s", query)
-    resp, err := http.Get(url)
-    if err != nil {
-        return PinterestResponse{}, err
-    }
-    defer resp.Body.Close()
+        url := fmt.Sprintf("https://horrid-api.vercel.app/pinterest?query=%s", query)
+        resp, err := http.Get(url)
+        if err != nil {
+                return PinterestResponse{}, err
+        }
+        defer resp.Body.Close()
 
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        return PinterestResponse{}, err
-    }
+        body, err := ioutil.ReadAll(resp.Body)
+        if err != nil {
+                return PinterestResponse{}, err
+        }
 
-    var result PinterestResponse
-    err = json.Unmarshal(body, &result)
-    if err != nil {
-        return PinterestResponse{}, err
-    }
+        var result PinterestResponse
+    
+        err = json.Unmarshal(body, &result)
+        if err != nil {
+                return PinterestResponse{}, err
+        }
 
-    return result, nil
+        return result, nil
 }
