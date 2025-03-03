0aafb0f5f899cc0f0be7ac9c9b72d30558773d5d
Mishal <131391958+Mishel-07@users.noreply.github.com>
2025-03-03 15:39:07 +0530

    Update bing.go

--- settings/bing.go
+++ settings/bing.go
@@ -1,36 +1,51 @@
-package settings 
+package settings
 
 import (
-        "encoding/json"
-        "fmt"
-        "io/ioutil"
-        "net/http"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
-type BingResponse struct {
-        Result []struct {
-                URL string `json:"images"`
-        } `json:"result"` 
+type ImageObject struct {
+	URL string `json:"url"` 
 }
 
-func SearchBing(query string, count int64) (BingResponse, error) {
-        url := fmt.Sprintf("https://horridapi.onrender.com/images?page=%d&query=%s", count, query)
-        resp, err := http.Get(url)
-        if err != nil {
-                return BingResponse{}, err
-        }
-        defer resp.Body.Close()
+func SearchBing(keyword string, nbImages int) ([]ImageObject, error) {
+	query := strings.ReplaceAll(keyword, " ", "+")
+	url := fmt.Sprintf("https://www.bing.com/images/search?q=%s", query)
+	
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return nil, err
+	}
 
-        body, err := ioutil.ReadAll(resp.Body)
-        if err != nil {
-                return BingResponse{}, err
-        } 
+	var imagesObjects []ImageObject
+	
+	doc.Find(".iusc").Each(func(i int, s *goquery.Selection) {
+		jsonContent, exists := s.Attr("m")
+		if exists {
+			var result map[string]interface{}
+			err := json.Unmarshal([]byte(jsonContent), &result)
+			if err == nil {
+				if murl, ok := result["murl"].(string); ok {					
+					imagesObjects = append(imagesObjects, ImageObject{URL: murl})
+				}
+			}
+		}
+	})
+	
+	if nbImages > 0 && nbImages < len(imagesObjects) {
+		imagesObjects = imagesObjects[:nbImages]
+	}
 
-        var result BingResponse
-        err = json.Unmarshal(body, &result)
-        if err != nil {
-                return BingResponse{}, err
-        }
-
-        return result, nil
+	return imagesObjects, nil
 }
