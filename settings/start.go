2b3bbca5251afaac6e509ca466517ce977e36b7c
Mishal <131391958+Mishel-07@users.noreply.github.com>
2025-03-07 13:30:51 +0530

    .

--- settings/start.go
+++ settings/start.go
@@ -22,9 +22,9 @@ func Start(b *gotgbot.Bot, ctx *ext.Context) error {
 			},
 		},
 	}              
-	_, e := message.Reply(b, "<b>Hey, I'm PinterestBot. You can search for Pinterest videos or photos, and you can download them too. I can also provide Google images, Bing images, etc.</b>\n\n<b>Commands:</b>\n\n/pinterest - search and download pinterest image\n/wallpaper - wallpaper search\n/img - from bing image\n\n<b>Tools:</b>\n\nSend me a Pinterest url I'll give that photo/video", &gotgbot.SendMessageOpts{ParseMode: gotgbot.ParseModeHTML, ReplyMarkup: buttons})    
-        if e != nil {
-		fmt.Println(e)
+	_, err := message.Reply(b, "<b>Hey, I'm PinterestBot. You can search for Pinterest videos or photos, and you can download them too. I can also provide Google images, Bing images, etc.</b>\n\n<b>Commands:</b>\n\n/pinterest - search and download pinterest image\n/wallpaper - wallpaper search\n/img - from bing image\n\n<b>Tools:</b>\n\nSend me a Pinterest url I'll give that photo/video", &gotgbot.SendMessageOpts{ParseMode: gotgbot.ParseModeHTML, ReplyMarkup: buttons})    
+        if err != nil {
+		fmt.Println(err)
 	}
 	return nil 
 }
