bb652537805adec631d2c4e7282f5b055a666131
Mishal <131391958+Mishel-07@users.noreply.github.com>
2025-09-04 11:31:39 +0530

    Update pinterestdl.go

--- settings/pinterestdl.go
+++ settings/pinterestdl.go
@@ -1,30 +1,30 @@
 package settings 
 
 import (
-        "encoding/json"
-        "io/ioutil"
-        "net/http"
+    "encoding/json"
+    "io/ioutil"
+    "net/http"
 )
 
 func PinterestDownload(link string) (string, error) {
-        url := "https://horridapi.onrender.com/download_pin?url=" + link
-        resp, err := http.Get(url)
-        if err != nil {
-                return "", err
-        }
-        defer resp.Body.Close()
+    url := "https://horridapi.onrender.com/download_pin?url=" + link
+    resp, err := http.Get(url)
+    if err != nil {
+        return "", err
+    }
+    defer resp.Body.Close()
  
-        body, err := ioutil.ReadAll(resp.Body)
-        if err != nil {
-                return "", err
-        }
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        return "", err
+    }
 
-        var data struct {
-                Link string `json:"link"`
-        }
-        err = json.Unmarshal(body, &data)
-        if err != nil {
-                return "", err
-        }
-        return data.Link, nil
+    var data struct {
+        Link string `json:"link"`
+    }
+    err = json.Unmarshal(body, &data)
+    if err != nil {
+        return "", err
+    }
+    return data.Link, nil
 }
